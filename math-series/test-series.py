46be6cfa3766fdd2b2456e25dedb0b9554b2ef8f
Allan Liebold <allan.liebold@gmail.com>
2017-10-18 10:08:16 -0700

    creates math-series folder and series.py and test-series.py

---
+++ math-series/test-series.py
@@ -0,0 +1,23 @@
+"""Tests Math Series Functions."""
+
+"""Tests for fib function
+Fibonacci Sequence: 0 1 1 2 3 5 8 13 21 34 55...
+"""
+
+
+def test_fib_1():
+    """Test fib with n of 1, should equal 0."""
+    from series import fib
+    assert fib(1) == 0
+
+
+def test_fib_2():
+    """Test fib with n of 2, should equal 1."""
+    from series import fib
+    assert fib(2) == 1
+
+
+def test_fib_5():
+    """Test fib with n of 5, should equal 3."""
+    from series import fib
+    assert fib(5) == 3
