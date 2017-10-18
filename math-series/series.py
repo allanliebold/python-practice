52fbeb2b66f15f7511c24f423b4d7eee2f76b0b7
Allan Liebold <allan.liebold@gmail.com>
2017-10-18 10:50:34 -0700

    creates sum_series function and tests

--- math-series/series.py
+++ math-series/series.py
@@ -27,3 +27,22 @@ def lucas(n):
         return 1
     else:
         return fib(n - 1) + fib(n + 1)
+
+"""Sum Series Function."""
+
+
+def sum_series(n, first=0, second=1):
+    """Return n in sequence.
+
+    User may provide first and second numbers.
+    """
+    if n == 0:
+        return 0
+    elif first == 0 and second == 1:
+        return fib(n)
+    elif first == 2 and second == 1:
+        return lucas(n)
+    else:
+        for i in range(1, n):
+            first, second = second, first + second
+        return first
--- math-series/test-series.py
+++ math-series/test-series.py
@@ -82,3 +82,55 @@ def test_lucas_string():
     from series import lucas
     with pytest.raises(TypeError):
         lucas('string')
+
+
+"""Sum Series Tests."""
+
+
+def test_sum_series_0():
+    """Test sum_series with n of 0, should return 0."""
+    from series import sum_series
+    assert sum_series(0) == 0
+
+
+def test_sum_series_501():
+    """Test sum_series with n of 5, first of 0, second of 1.
+
+    Should return 3
+    """
+    from series import sum_series
+    assert sum_series(5, 0, 1) == 3
+
+
+def test_sum_series_521():
+    """Test sum_series with n of 5, first of 2, second of 1.
+
+    Should return 7
+    """
+    from series import sum_series
+    assert sum_series(5, 2, 1) == 7
+
+
+def test_sum_series_555():
+    """Test sum_series with n of 5, first of 5, second of 5.
+
+    Should return 25
+    """
+    from series import sum_series
+    assert sum_series(5, 5, 5) == 25
+
+
+def test_sum_series_102030():
+    """Test sum_series with n of 10, first of 20, second of 30.
+
+    Should return 1410
+    """
+    from series import sum_series
+    assert sum_series(10, 20, 30) == 1440
+
+
+def test_sum_series_string():
+    """Test sum_series with string, should raise TypeError."""
+    from series import sum_series
+    with pytest.raises(TypeError):
+        sum_series('string')
